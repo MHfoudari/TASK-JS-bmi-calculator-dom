52ce1070443d4a2ad0a5514a592e8ca4a7a95bfc
MHfoudari <eng.mohammed.alfoudari@gmail.com>
2023-05-08 21:18:40 +0300

    BMI with description

--- index.js
+++ index.js
@@ -8,8 +8,21 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
-  
-let BMI = (weight / ((height/100)^2))
-alert("Your BMI is"+BMI)
+let heightinmeter = height/100;
+let BMI = weight / ((heightinmeter*heightinmeter));
+alert("Your BMI is "+BMI)
 
-}
+// Below 18.5 Underweight
+// 18.5 – 24.9 Healthy Weight
+// 25.0 – 29.9 Overweight
+// 30.0 and Above Obesity
+// show the BMI state in an alert
+if (BMI <18.5) {
+  alert("Your BMI is  "+BMI+"  Underweight")}
+  else if (BMI>=18.5 && BMI <=24.9)
+    alert("Your BMI is  "+BMI+" Healthy")
+  else if (BMI>=25.0 && BMI <=29.9) {
+      alert("Your BMI is  "+BMI+"  Overweight")}
+      else if (BMI>=30) {
+        alert("Your BMI is "+BMI+"  Obese")}
+      } 
